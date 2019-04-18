21005a055115339ce69cee394c21fc62aa2dddd0
BernardoRodrigues <bernardo.qtr.21@gmail.com>
2019-04-18 03:15:07 +0100

    finished yama-server module

--- yama-server.js
+++ yama-server.js
@@ -1,9 +1,22 @@
 const router = require('./lib/router/router')
 const api = require('./lib/yama-web-api')
 const http = require('http')
+const args = process.argv
+
+if (args.length !== 1) {
+    throw new Error("Port number must be passed")
+}
+
+const port = parseInt(args.pop())
 
 router.get('/artists/search/:name', api.getArtists)
 router.get('/artist/:mbid/albums', api.getTopAlbums)
 router.get('/albums/:mbid', api.getAlbumInfo)
+router.post('/playlist', api.createPlaylist)
+router.delete('/playlist/:playlistId', api.deletePlaylist)
+router.get('/playlists', api.getPlaylists)
+router.get('/playlist/:playlistId', api.getPlaylistInfo)
+router.put('/playlist/:playlistId/track', api.addMusicToPlaylist)
+router.delete('/playlist/:playlistId/track', api.removeMusicFromPlaylist)
 
-http.createServer(router).listen(8080, () => console.log("server started on port 8080"))
+http.createServer(router).listen(port, () => console.log(`server started on port ${port}`))
