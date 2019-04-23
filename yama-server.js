ffbf562f0dea49af6c8d1d9eaf887b18ca91639c
DanielFeliciano <DanielFeliciano@users.noreply.github.com>
2019-04-24 00:46:46 +0100

    bug fixing endpoints working - artists and albums added Postman collection for working endpoints

--- yama-server.js
+++ yama-server.js
@@ -1,22 +1,25 @@
+'use strict'
 const router = require('./lib/router/router')
 const api = require('./lib/yama-web-api')
 const http = require('http')
 const args = process.argv
 
-if (args.length !== 1) {
-    throw new Error("Port number must be passed")
+if (args.length !== 3) {
+    //throw new Error("Port number must be passed")
+    args.push('3000')
 }
 
 const port = parseInt(args.pop())
 
 router.get('/artists/search/:name', api.getArtists)
-router.get('/artist/:mbid/albums', api.getTopAlbums)
+router.get('/artists/:mbid/albums', api.getTopAlbums)
 router.get('/albums/:mbid', api.getAlbumInfo)
-router.post('/playlist', api.createPlaylist)
-router.delete('/playlist/:playlistId', api.deletePlaylist)
+router.post('/playlists', api.createPlaylist)
+router.delete('/playlists/:playlistId', api.deletePlaylist)
 router.get('/playlists', api.getPlaylists)
-router.get('/playlist/:playlistId', api.getPlaylistInfo)
-router.put('/playlist/:playlistId/track', api.addMusicToPlaylist)
-router.delete('/playlist/:playlistId/track', api.removeMusicFromPlaylist)
+router.get('/playlists/:playlistId', api.getPlaylistInfo)
+router.put('/playlists/:playlistId/track', api.addMusicToPlaylist)
+router.delete('/playlists/:playlistId/track', api.removeMusicFromPlaylist)
 
-http.createServer(router).listen(port, () => console.log(`server started on port ${port}`))
+http.createServer(router)
+    .listen(port, () => console.log(`server started on port ${port}`))
