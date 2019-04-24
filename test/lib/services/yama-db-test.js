1c6e1020b853f271f1deb0296b63e9ea8e57d1a6
BernardoRodrigues <bernardo.qtr.21@gmail.com>
2019-04-24 14:43:36 +0100

    fixed bugs. added mappings and models

--- test/lib/services/yama-db-test.js
+++ test/lib/services/yama-db-test.js
@@ -1,8 +1,8 @@
 const assert = require('assert')
 const service = require('./../../../lib/services/yama-db')
 
-describe('', function() {
+describe('Yama Db Create Playlist', function() {
     it('', function() {
-        service.createPlaylist("xpto", "something", console.log)
+        service.createPlaylist("xpto", "something", (result) => service.deletePlaylist(result._id))
     })
 })
--- yama-server.js
+++ yama-server.js
@@ -1,13 +1,16 @@
 const router = require('./lib/router/router')
-const api = require('./lib/yama-web-api')
+const lastFmService = require('./lib/services/lastfm-data')
+const db = require('./lib/services/yama-db')
+const services = require('./lib/yama-services')(lastFmService, db)
+const api = require('./lib/yama-web-api')(services)
 const http = require('http')
 const args = process.argv
 
-if (args.length !== 1) {
-    throw new Error("Port number must be passed")
-}
+// if (args.length !== 1) {
+//     throw new Error("Port number must be passed")
+// }
 
-const port = parseInt(args.pop())
+const port = 8080
 
 router.get('/artists/search/:name', api.getArtists)
 router.get('/artist/:mbid/albums', api.getTopAlbums)
@@ -18,5 +21,6 @@ router.get('/playlists', api.getPlaylists)
 router.get('/playlist/:playlistId', api.getPlaylistInfo)
 router.put('/playlist/:playlistId/track', api.addMusicToPlaylist)
 router.delete('/playlist/:playlistId/track', api.removeMusicFromPlaylist)
+router.error(api.notFound)
 
-http.createServer(router).listen(port, () => console.log(`server started on port ${port}`))
+http.createServer(router.findEndpoint).listen(port, () => console.log(`server started on port ${port}`))
