f211635167ac97faf8ffb8571eb84555ebd5e8fb
BernardoRodrigues <bernardo.qtr.21@gmail.com>
2019-05-18 05:50:36 +0100

    fixed bugs

--- yama-server.js
+++ yama-server.js
@@ -1,33 +1,43 @@
 'use strict'
 
 const morgan = require('morgan')
+const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 const rp = require('request-promise')
 const lastFmService = require('./lib/services/lastfm-data')(rp)
 const db = require('./lib/services/yama-db')(rp)
 const serviceDb = require('./lib/yama-db-service')(db)
-const apiService = require('./lib/yama-lastfm-service')(lastFmService)
+const serviceArtist = require('./lib/yama-lastfm-service')(lastFmService)
 const playlistApi = require('./lib/yama-web-api-playlists')(serviceDb, express.Router())
-const artistApi = require('./lib/yama-web-api-artist')(apiService, express.Router())
+const artistApi = require('./lib/yama-web-api-artist')(serviceArtist, express.Router())
 
 const args = process.argv
 
 if (args.length !== 3) {
     throw new Error("Port number must be passed")
 }
-app.use(morgan)
+
+const port = Number(args[2])
+
+app.use(morgan('tiny'))
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(artistApi, playlistApi)
+app.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
+app.use('/playlist', playlistApi)
+app.use('/artist', artistApi)
 app.use(notFound)
 
 function notFound(req, resp) {
-    resp.statusCode = 404
-    resp.setHeader('Content-Type', 'application/json')
-    resp.end(JSON.stringify({
+    const err = {
         message: "Method or Path do not exist",
         statusCode: 404
-    }))
+    }
+    resp
+        .status(404)
+        .type('json')
+        .end(JSON.stringify(err))
 }
+
 app.listen(port, () => console.log(`server started on port ${port}`))
