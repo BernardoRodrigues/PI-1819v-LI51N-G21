1545c62357df09361a5b686a86006db7e6072e69
BernardoRodrigues <bernardo.qtr.21@gmail.com>
2019-07-19 00:35:22 +0100

    fixed sign-up fetch body issue

--- yama-app/app/scripts/sign-up.js
+++ yama-app/app/scripts/sign-up.js
@@ -1,33 +1,51 @@
-module.exports = async function(alertScript, alertTemplate) {
-    console.log('Sign Up Script ')
+module.exports = async function (alertScript, alertTemplate) {
     return new Promise((resolve, reject) => {
-        const options = {
-            method: 'POST'
-        }
-        document.getElementById('sendSignUpBtn').onclick = () => {
-                console.log('sending sign up request')
-                const username = document.getElementById('signUpUsername').value
-                const password = document.getElementById('signUpPassword').value
-                options['body'] = {
+        document.getElementById('sendSignUpBtn').onclick = function() {
+            console.log('sending sign up request')
+            const username = document.getElementById('signUpUsername').value
+            const password = document.getElementById('signUpPassword').value
+            console.log({
+                username: document.getElementById('signUpUsername'),
+                password: document.getElementById('signUpPassword')
+            })
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
                     username: username,
                     password: password
-                }
-                fetch('api/v1.0.0/auth/sign-up', options)
-                    .then(
-                        (value) => {
-                        alertScript(alertTemplate, {message: 'Welcome!', type: 'success'})
+                })
+            }
+            console.log({
+                options: options
+            })
+            fetch('api/v1.0.0/auth/sign-up', options)
+                .then(
+                    (value) => {
+                        alertScript(alertTemplate, {
+                            message: 'Welcome!',
+                            type: 'success'
+                        })
                         // const lastHash = localStorage.getItem('lastHash')
                         window.location.hash = '#welcome'
                         resolve()
-                    }, 
+                    },
                     (err) => {
                         if (err.status && err.status >= 500) {
-                            alertScript(alertTemplate, {message: 'There was a server error. We\'re very sorry!', type: 'error'})
+                            alertScript(alertTemplate, {
+                                message: 'There was a server error. We\'re very sorry!',
+                                type: 'error'
+                            })
                         } else {
-                            alertScript(alertTemplate, {message: 'These credentials are already being used', type: 'error'})
+                            alertScript(alertTemplate, {
+                                message: 'These credentials are already being used',
+                                type: 'error'
+                            })
                         }
                         reject()
                     })
-            }
+        }
     })
 }
